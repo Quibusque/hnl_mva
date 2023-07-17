d8ed9250f16f25c88876782f3bc5e20318f29518
Tommaso93 <tommaso.diotalevi@gmail.com>
2023-07-17 15:25:18 +0200

    Implement multi column plots with one uproot input
    Some code cleaning, moving scripts in a dedicated folder.
    Now it reads multiple columns with a single input files loading.
    Changed aesthetics to the CMS ROOT format, using mplhep package.
    
    Update readme
    
    Move script to specific folder
    
    Move file to script folder
    
    Create new function for uproot variable loading
    
    Implement loading multiple columns with one input
    
    Fix plotting for multiple columns and aesthetics
    
    Move file to script folder
    
    Implement new logic and add argparsing
    
    Test with two columns
    
    Add configurable output for config
    
    Fix argument caption
    
    Fix README
    
    Update README.md

--- analyze.py
+++ analyze.py
@@ -1,11 +1,23 @@
-from hnl_mva_tools import *
+import argparse
+from script.hnl_mva_tools import read_json_file, plot, load_variables
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--outputdir', type=str, default='plots', help= 'Name of the output plot directory, defaults to "plots"')
+    args = parser.parse_args()
 
     ntuples = read_json_file("cfg/ntuples.json")
     histograms = read_json_file("cfg/histograms.json")
     treename = "final_tree"
     weight_name = "tot_weight"
 
+    headers = [hdr["column_name"] for hdr in histograms]
+    bkg_labels = [s[s.rfind("QCD_Pt-"):s.rfind("_MuEnriched")] for s in ntuples["background"]]
+   
+   #Load variables from ROOT files
+    signal, background, sig_weight, bkg_weight = load_variables(ntuples, headers, treename, weight_name)
+    
+    #Plot variables
     for histo in histograms:
-        plot(ntuples,treename,weight_name,histo)
+        plot(histo,signal,background,sig_weight,bkg_weight,bkg_labels=bkg_labels,out_dir=args.outputdir)
